ef0df3e37401eeea0eb9b3178f4b84a12d4187eb
Sungmin Kim <144216447+smk1099@users.noreply.github.com>
2024-03-12 16:30:29 +0900

    Title: 트리 순회법 변경 2, Time: 8ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240312/트리 순회법 변경 2/change-tree-traversal-2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+전위 순회에서 첫번째 수는 루트
+중위 순회에서 루트 앞으로는 왼쪽 서브트리, 루트 뒤로는 오른쪽 서브트리
+후위 순회에서는 왼쪽 서브트리 오른쪽 서브트리 루트 순으로 탐색.
+*/
+
+vector<int> pre_order(100'001);
+vector<int> in_order(100'001);
+vector<int> post_order(100'001);
+int n;
+int cnt = 1;
+void set_tree(int start, int end, int in_start, int in_end){
+    //전위 순회에서 시작과 끝, 중위 순회에서 시작과 끝.
+    if(start > end) return;
+    if(start == end){
+        post_order[cnt++] = pre_order[start];
+        return;
+    }
+
+
+    int left_subtree_length = 0;
+    for(int i = in_start; i <= in_end; i++){
+        if(pre_order[start] == in_order[i]){
+            break;
+        }
+        left_subtree_length++;
+    }
+
+    set_tree(start + 1, start + left_subtree_length, in_start, in_start + left_subtree_length);
+    //왼쪽 서브트리
+    set_tree(start + left_subtree_length + 1, end, in_start + left_subtree_length + 2, in_end);
+    //오른쪽 서브트리
+
+    //루트를 마지막으로 탐색
+    post_order[cnt++] = pre_order[start];
+}
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        cin >> pre_order[i];
+    }
+    for(int i = 1; i <= n; i++){
+        cin >> in_order[i];
+    }
+
+    set_tree(1, n, 1, n);
+
+    for(int i = 1; i <= n; i++){
+        cout << post_order[i] << " ";
+    }
+    
+    return 0;
+}
