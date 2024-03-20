1cae5065601658460bf31883df7a2b9e4c210c8d
Sungmin Kim <144216447+smk1099@users.noreply.github.com>
2024-03-20 13:12:02 +0900

    Title: 두 개의 동일한 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240320/두 개의 동일한 수열/two-equal-series.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(null);
+
+    unordered_map<int, int> m;
+    int n;
+    cin >> n;
+    string answer = "Yes";
+    for(int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        m[num]++;
+    }
+    for(int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        if(m[num] != 1){
+            answer = "No";
+            break;
+        }
+    }
+    cout << answer;
+    return 0;
+}
