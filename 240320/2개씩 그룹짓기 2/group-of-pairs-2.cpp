f9e3d638ead82f8b357984e4a976e24bfcb593fc
Sungmin Kim <144216447+smk1099@users.noreply.github.com>
2024-03-20 19:15:14 +0900

    Title: 2개씩 그룹짓기 2, Time: 57ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240320/2개씩 그룹짓기 2/group-of-pairs-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[100000];
+    int answer = INT_MAX;
+    cin >> n;
+    for(int i = 0; i < n * 2; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr + n * 2);
+    for(int i = 0; i < n; i++){
+        answer = min(answer, arr[n + i] - arr[i]);
+    }
+    cout << answer;
+    return 0;
+}
