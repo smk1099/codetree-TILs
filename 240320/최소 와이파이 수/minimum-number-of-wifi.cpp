19d742e819af9a0255d10739a264d26c0572cc7c
Sungmin Kim <144216447+smk1099@users.noreply.github.com>
2024-03-20 17:48:03 +0900

    Title: 최소 와이파이 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240320/최소 와이파이 수/minimum-number-of-wifi.cpp
+++ 240320/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main() {
     int n, m;
+    int arr[100];
     cin >> n >> m;
     int answer = 0;
     for(int i = 0; i < n; i++){
-        int num;
-        cin >> num;
-        if(num == 1){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 1){
             answer++;
             i += 2 * m;
         }
