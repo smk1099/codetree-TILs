583a403fea709b1cb64023c5e319e76cfeea466a
Sungmin Kim <144216447+smk1099@users.noreply.github.com>
2024-03-21 15:10:36 +0900

    Title: 강력한 폭발, Time: 26ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/강력한 폭발/strong-explosion.cpp
+++ 240321/강력한 폭발/strong-explosion.cpp
@@ -63,6 +63,8 @@ void choose_num(int index){
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> n;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
