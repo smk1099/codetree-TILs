9bdb71e61c709c439543e8f41be71b496aef174c
Sungmin Kim <144216447+smk1099@users.noreply.github.com>
2024-03-31 16:15:45 +0900

    Title: 1이 3개 이상 있는 위치, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240331/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 240331/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int count = 0;
+int answer = 0;
 int matrix[101][101];
-int visited[101][101];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int n;
@@ -13,34 +13,33 @@ bool in_range(int x, int y){
     return x >= 0 && x < n && y >= 0 && y < n;
 }
 
-void dfs(int x, int y, int number){
-    visited[x][y] = number;
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(in_range(nx, ny) && matrix[nx][ny] == 1 && visited[nx][ny] == 0){
-            dfs(nx, ny, number);
-        }
-    }
-}
+
 int main() {
     cin >> n;
     for(int i= 0; i < n; i++){
         for(int j = 0; j < n; j++){
             cin >> matrix[i][j];
-            visited[i][j] = 0;
         }
     }
 
     for(int i= 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            if(matrix[i][j] == 1 && visited[i][j] == 0){
-                dfs(i, j, ++count);
+            count = 0;
+            for(int k = 0; k < 4; k++){
+                int nx = i + dx[k];
+                int ny = j + dy[k];
+                if(in_range(nx, ny) && matrix[nx][ny] == 1){
+                    count++;
+                }
+            }
+            if(count >= 3){
+                answer++;
             }
         }
     }
+    
 
-    cout << count;
+    cout << answer;
 
 
 
