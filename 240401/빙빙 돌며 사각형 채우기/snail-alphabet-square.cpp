920ba6e35c4e9f4897744cae7b99f0721e8a1165
Sungmin Kim <144216447+smk1099@users.noreply.github.com>
2024-04-01 20:33:55 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240401/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
+++ 240401/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -29,7 +29,7 @@ int main() {
     }
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < m; j++){
             cout << (char)((matrix[i][j] - 1) % 26 + 'A') << " ";
         }
         cout << "\n";
